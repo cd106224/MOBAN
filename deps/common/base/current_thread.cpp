b6aeb619c8787f82d37d3a7261800d248ebe74d3
cd106224 <1062247298@qq.com>
2025-07-30 21:42:47 +0800

    [FEA] 支持win下的MinGW编译 (#10)
    * refactor(deps): 重构部分代码并添加 Windows 支持- 注释掉 CMakeLists.txt 中的 GPU 相关代码- 添加 Windows 下的线程 ID 获取和日志输出支持
    - 修改 current_thread 以支持 Windows平台
    - 优化 common_logger 日志输出格式
    
    * build(CMakeLists): 更新构建系统以支持不同平台
    
    - 添加对 Windows 和 Linux 系统的判断和处理
    - 移除 GPU 编译环境检测和 clang_format 配置
    - 新增 find_thread.cmake 文件以替代直接使用 Threads 包
    - 删除 ThreadLocal 类的实现

--- deps/common/base/current_thread.cpp
+++ deps/common/base/current_thread.cpp
@@ -1,35 +1,46 @@
 #include "current_thread.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <sys/syscall.h>
-
-#include <cstdio>
-
-#include "thread_local.h"
 #include "unistd.h"
+#endif
+
+#include <cinttypes>
+#include <cstdio>
+#include <cstring>
 
 namespace current_thread {
 namespace detail {
-inline pid_t gettid() noexcept {
+inline int64_t gettid() noexcept {
+#ifdef _WIN32
+  return GetCurrentThreadId();
+#else
   return static_cast<pid_t>(::syscall(SYS_gettid));
+#endif
 }
 struct Cache {
-  pid_t tid{};
+  int64_t tid{};
   char tidStr[32]{};
-  std::string threadName = "unKnown";
+  char threadName[32] = "unKnown";
   Cache() {
     tid = gettid();
-    std::snprintf(tidStr, sizeof(tidStr), "%5d ", tid);
+    std::snprintf(tidStr, sizeof(tidStr), "%6d" PRId64, tid);
   }
 };
-inline ThreadLocal<Cache>& cache() noexcept {
-  static ThreadLocal<Cache> cache;
+inline Cache& cache() noexcept {
+  static thread_local Cache cache;
   return cache;
 }
 }  // namespace detail
-pid_t id() noexcept { return detail::cache()->tid; }
+int64_t id() noexcept { return detail::cache().tid; }
 bool isMain() noexcept { return id() == detail::gettid(); }
 void set_thread_name(const std::string& name) noexcept {
-  detail::cache()->threadName = name;
+  constexpr std::size_t kMax = sizeof(detail::cache().threadName);
+  auto* buf = detail::cache().threadName;  // 只取一次指针
+  std::strncpy(buf, name.c_str(), kMax - 1);
+  buf[kMax - 1] = '\0';
 }
-std::string thread_name() noexcept { return detail::cache()->threadName; }
+std::string thread_name() noexcept { return detail::cache().threadName; }
 }  // namespace current_thread
--- deps/common/base/current_thread.h
+++ deps/common/base/current_thread.h
@@ -1,9 +1,10 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 namespace current_thread {
-pid_t id() noexcept;  // 缓存的 tid
+int64_t id() noexcept;  // 缓存的 tid
 bool isMain() noexcept;
 void set_thread_name(const std::string &name) noexcept;
 std::string thread_name() noexcept;
